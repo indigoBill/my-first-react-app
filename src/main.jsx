014650d09264ae8736b2d9547b971cfb17ff3dc0
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-12-06 05:57:32 -0500

    Call flower list component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { Greeting, FoodGreeting, HtmlToJsx } from './Greeting.jsx'
+// import App from './App.jsx'
+// import { Greeting, FoodGreeting, HtmlToJsx } from './Greeting.jsx'
+import DisplayLists from './List.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* 
     <App />
     <Greeting/>
     <FoodGreeting/>
-    <HtmlToJsx/>
-  </StrictMode>,
+    <HtmlToJsx/> 
+    */}
+
+    <DisplayLists />
+  </StrictMode>
 )
